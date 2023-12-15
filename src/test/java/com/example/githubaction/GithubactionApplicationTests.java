ef775864064ddd75033e9dead86e4accbbaf51be
Jihoyakim <kjh970201@gmail.com>
2023-12-15 14:34:01 +0900

    gitgub action text1

---
+++ src/test/java/com/example/githubaction/GithubactionApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.githubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
+
+@SpringBootTest
+class GithubactionApplicationTests {
+
+    @Test
+    public void test1() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("성공했어요!");
+    }
+
+}
